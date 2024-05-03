2a28413bd699685007252281b6f4fe64a36e8f78
Mugen <52742495+eyimofe-olasoko@users.noreply.github.com>
2024-05-03 13:36:44 +0100

    Adding cards to the player's hand
    Adding cards to the players hand that they choose from the playing cards

--- Solitaire/Solitaire/Solitaire.cpp
+++ Solitaire/Solitaire/Solitaire.cpp
@@ -20,18 +20,17 @@ int main()
     //Creating a variable to track the number of cards in the card vectors
     int changeLength = 5;
 
-
     //Setting the seed of srand to 0 so a different number generates on each run of the game.
     srand(time(0));
 
     //Creating a deck object based on the deck class
     Deck solitaireDeck = Deck();
 
-    //Creating a vector to store the player's cards
-    vector<Cards> playerHand;
-
     //Creating a vector to store the card pile that the player can draw from
     vector<Cards> cardStock;
+
+    //Creating a vector to store the player's cards
+    vector<Cards> playerHand;
 
     //Shuffles card deck
     solitaireDeck.shuffle();
@@ -44,6 +43,7 @@ int main()
 
     cout << endl;
 
+    //Adding 17 cards to stock pile
     for (size_t i = 0; i < 17; i++)
     {
         cardStock.push_back(solitaireDeck.cardDeck.back());
@@ -53,8 +53,36 @@ int main()
 
     do
     {
-        //Diplaying the player starting card/getting card choice input
-        cout << "Your staring card is: " << solitaireDeck.cardDeck.back().getValue() << " of " << solitaireDeck.cardDeck.back().getSuit() << endl;
+
+        cout << endl;
+
+        //Checking if card value matches card name
+        if (solitaireDeck.cardDeck.back().getValue() == 0)
+        {
+            solitaireDeck.cardDeck.back().setValue(1);
+
+            cout << "Your starting card is: " << "Ace" << " of " << solitaireDeck.cardDeck.back().getSuit() << endl;
+        }
+        else if (solitaireDeck.cardDeck.back().getValue() == 0)
+        {
+            cout << "Your starting card is: " << "Ace" << " of " << solitaireDeck.cardDeck.back().getSuit() << endl;
+        }
+        else if (solitaireDeck.cardDeck.back().getValue() == 11)
+        {
+            cout << "Your starting card is: " << "Jack" << " of " << solitaireDeck.cardDeck.back().getSuit() << endl;
+        }
+        else if (solitaireDeck.cardDeck.back().getValue() == 12)
+        {
+            cout << "Your starting card is: " << "Queen" << " of " << solitaireDeck.cardDeck.back().getSuit() << endl;
+        }
+        else if (solitaireDeck.cardDeck.back().getValue() == 13)
+        {
+            cout << "Your starting card is: " << "King" << " of " << solitaireDeck.cardDeck.back().getSuit() << endl;
+        }
+        else
+        {
+            cout << "Your starting card is: " << solitaireDeck.cardDeck.back().getValue() << " of " << solitaireDeck.cardDeck.back().getSuit() << endl;
+        }
 
         cout << "Choose a card: ";
 
@@ -77,11 +105,14 @@ int main()
         while (playerChoice > 0 || playerChoice <= 5)
         {
 
-            //Checking if the player chose the first card 
+            //Checking what card (playerChoice) and row (playerChoiceRow) the player choice
             if (playerChoice == 1 && playerChoiceRow == 5)
             {
-                //Deleting the first card the vector 
-                playingCards.fifthRow.erase(playingCards.fifthRow.begin());
+                //Adding the playerChoice card to their hand
+                playerHand.push_back(playingCards.fifthRow[4]);
+
+                //Deleting the card as the index corresponding with the playerChoice
+                playingCards.fifthRow.pop_back();
 
                 //Decrementing the vector length by one 
                 playingCards.fifthLength--;
@@ -93,7 +124,10 @@ int main()
             }
             else if (playerChoice == 2 && playerChoiceRow == 5)
             {
-                playingCards.fifthRow.erase(playingCards.fifthRow.begin() + 1);
+                //Adding the playerChoice card to their hand
+                playerHand.push_back(playingCards.fifthRow[3]);
+
+                playingCards.fifthRow.erase(playingCards.fifthRow.begin() + 3);
 
                 //Decrementing the vector length by one 
                 playingCards.fifthLength--;
@@ -104,6 +138,9 @@ int main()
             }
             else if (playerChoice == 3 && playerChoiceRow == 5)
             {
+                //Adding the playerChoice card to their hand
+                playerHand.push_back(playingCards.fifthRow[2]);
+
                 playingCards.fifthRow.erase(playingCards.fifthRow.begin() + 2);
 
                 //Decrementing the vector length by one 
@@ -115,7 +152,10 @@ int main()
             }
             else if (playerChoice == 4 && playerChoiceRow == 5)
             {
-                playingCards.fifthRow.erase(playingCards.fifthRow.begin() + 3);
+                //Adding the playerChoice card to their hand
+                playerHand.push_back(playingCards.fifthRow[1]);
+
+                playingCards.fifthRow.erase(playingCards.fifthRow.begin() + 1);
 
                 //Decrementing the vector length by one 
                 playingCards.fifthLength--;
@@ -126,7 +166,10 @@ int main()
             }
             else if (playerChoice == 5 && playerChoiceRow == 5)
             {
-                playingCards.fifthRow.erase(playingCards.fifthRow.begin() + 4);
+                //Adding the playerChoice card to their hand
+                playerHand.push_back(playingCards.fifthRow[0]);
+
+                playingCards.fifthRow.erase(playingCards.fifthRow.begin());
 
                 //Decrementing the vector length by one 
                 playingCards.fifthLength--;
@@ -136,6 +179,11 @@ int main()
                 cout << endl;
             }
 
+            if (playerChoice == 1 && playerChoiceRow == 4)
+            {
+                
+            }
+            
             cout << endl;
 
             cout << "Choose a card: ";
